4446a38718da0d79e7f0e1439f6ae3283922f78e
mayura1996 <mmayurapavan@gmail.com>
2020-12-20 22:29:56 +0530

    routes done

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.get("/about",function(req,res){
   res.send("Im a Computer Engineering Undergraduate");
 })
 
+app.get("/hobbies",function(req,res){
+  res.send("<ul><li>coffee</li><li>tea</li></ul>");
+})
 
 app.listen(3000, function(){
   console.log("Server started on port 3000")

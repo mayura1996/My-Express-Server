7c01aac79e949c8bab0af7260ea552889419644f
mayura1996 <mmayurapavan@gmail.com>
2020-12-20 22:14:40 +0530

    routes

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+
+app.get("/",function(req,res){
+  res.send("<h1>Hello, World!</h1>");
+});
+
+app.get("/contact",function(req,res){
+  res.send("Contact me at: mmayurapavan@gmail.com");
+})
+
+app.get("/about",function(req,res){
+  res.send("Im a Computer Engineering Undergraduate");
+})
+
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000")
+});
